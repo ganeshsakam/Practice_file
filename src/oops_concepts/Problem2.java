800e609a3567eac85b8891faf561f98c74aea65d
Your Name <you@example.com>
2023-10-05 12:05:09 +0530

    date 05/10

---
+++ src/oops_concepts/Problem2.java
@@ -0,0 +1,9 @@
+package oops_concepts;
+/**
+ * Write a Java program to create a class known as "BankAccount" with methods called deposit() and withdraw().
+ *  Create a subclass called SavingsAccount that overrides the withdraw() method to prevent 
+ *  withdrawals if the account balance falls below one hundred.
+ */
+public class Problem2 {
+
+}
